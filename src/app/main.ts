82786714d7ff2fde2599d0dfb10ca4a602b761f1
elujambio <eugeniolujambio@gmail.com>
2017-01-15 13:53:34 -0600

    menu now shows

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,4 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { MenuController } from 'ionic-angular';
 
+
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
